b5494970dfa3938ddd25cab441ee696dcf01a18f
Fn-M <96444305+Fn-M@users.noreply.github.com>
2025-07-13 22:54:35 +0200

    Enabing deployment through GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/availability-calendar/', // GitHub Pages base path
   server: {
     proxy: {
       '/api': {
